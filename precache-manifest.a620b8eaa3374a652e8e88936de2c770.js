6b30d969872850277d67a36056293446251070bc
inoutw <qingmin.ai@foxmail.com>
2020-08-08 16:29:07 +0800

    Updates

---
+++ precache-manifest.a620b8eaa3374a652e8e88936de2c770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd5761192085339642e09dfb448ab61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc224564fc6fdeb9de3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12d089d935438c4357f1",
+    "url": "/static/js/2.365b9421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.365b9421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc224564fc6fdeb9de3",
+    "url": "/static/js/main.a5fca142.chunk.js"
+  },
+  {
+    "revision": "9ed45c4b3cb74c46cfa4",
+    "url": "/static/js/runtime-main.5a584dbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
